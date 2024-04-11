0ef7e1fdd99505a813113f0040508e2f0a96503d
Shuairan Chen <145292117+shuairanchen@users.noreply.github.com>
2024-04-11 10:38:55 -0500

    Fix the warnings

--- preprocess_pdf.py
+++ preprocess_pdf.py
@@ -1,27 +1,13 @@
-import os
 import fitz
 import re
 import os
-import sys
-import spacy
 import tiktoken
-import openai
-from langchain.llms import OpenAI
 from langchain_community.document_loaders import TextLoader, PyPDFLoader
-from langchain.indexes import VectorstoreIndexCreator
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
-from langchain.vectorstores import Pinecone as PC
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from pinecone import Pinecone
 from pinecone import ServerlessSpec
 import time
-# import gradio as gr
-import shutil
-import tempfile
 from tqdm.auto import tqdm
 from uuid import uuid4
 
@@ -33,8 +19,6 @@ pinecone_api_key = os.getenv("PINECONE_API_KEY")
 index_name = "coms579pdfqa"
 model_name = "text-embedding-ada-002"
 embedder = OpenAIEmbeddings(model=model_name, openai_api_key=openai_api_key)
-llm = ChatOpenAI(api_key=openai_api_key, model_name="gpt-3.5-turbo", temperature=0.5, max_tokens=1000, top_p=0.9,
-                 frequency_penalty=0.0, presence_penalty=0.0)
 pc = Pinecone(api_key=pinecone_api_key)
 spec = ServerlessSpec(cloud="GCP", region="us-central1")
 
--- user_query.py
+++ user_query.py
@@ -1,10 +1,9 @@
 import os
 import tiktoken
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from langchain.vectorstores import Pinecone as PC
+from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone
 from pinecone import ServerlessSpec
 
@@ -16,8 +15,6 @@ pinecone_api_key = os.getenv("PINECONE_API_KEY")
 index_name = "coms579pdfqa"
 model_name = "text-embedding-ada-002"
 embedder = OpenAIEmbeddings(model=model_name, openai_api_key=openai_api_key)
-llm = ChatOpenAI(api_key=openai_api_key, model_name="gpt-3.5-turbo", temperature=0.5, max_tokens=1000, top_p=0.9,
-                 frequency_penalty=0.0, presence_penalty=0.0)
 pc = Pinecone(api_key=pinecone_api_key)
 spec = ServerlessSpec(cloud="GCP", region="us-central1")
 
@@ -37,9 +34,15 @@ def get_exists_index_from_pinecone(index_name):
 
 def create_chat_model(index):
     text_field = "text"
-    vectorstore = PC(index, embedder, text_field)
+    vectorstore = PineconeVectorStore(index, embedder, text_field)
     retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 5})
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True, max_length=1000)
+    model_kwargs = {
+        "top_p": 0.9,
+        "frequency_penalty": 0.0,
+        "presence_penalty": 0.0
+    }
+    llm = ChatOpenAI(api_key=openai_api_key, model_name="gpt-3.5-turbo", temperature=0.5, max_tokens=1000, model_kwargs=model_kwargs)
     chat = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory)
     return chat
 
@@ -47,8 +50,10 @@ def create_chat_model(index):
 def get_answer_from_chat_model(question):
     index = get_exists_index_from_pinecone(index_name)
     chat = create_chat_model(index)
-    dic = {"question": question}
-    response = chat(dic)
+    dic = {
+        'question': question,
+    }
+    response = chat.invoke(dic)
     answer = response["answer"]
     return answer
 
@@ -56,9 +61,10 @@ def get_answer_from_chat_model(question):
 def command_running():
     while True:
         print("Please input your question")
+        print("User: ", end="")
         question = str(input())
         answer = get_answer_from_chat_model(question)
-        print("Chatbot: ", answer)
+        print("Chatbot:", answer)
 
         print("Do you want to ask another question? (y/n)")
         response = input()
