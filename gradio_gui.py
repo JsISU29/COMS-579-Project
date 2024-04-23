0021cca7eb02fea65db1b6aae99f6031aa0756a1
Shuairan Chen <145292117+shuairanchen@users.noreply.github.com>
2024-04-22 20:05:59 -0500

    Update Rerank top_k and Gradio GUI

--- gradio_gui.py
+++ gradio_gui.py
@@ -4,8 +4,23 @@ from preprocess_pdf import upload_index_pdf_to_pinecone
 from user_query import get_answer_from_chat_model
 
 
+top_k = 5
+authors_text = """
+<div style='font-size: 12px;'>
+  <p>Authors: Shuairan Chen, Fan Zhou, Jian Sun</p>
+  <p>Upload your PDF files and ask questions about the content.</p>
+</div>
+"""
+
+
+def update_top_k(val):
+    global top_k
+    top_k = val
+    return top_k
+
+
 def slow_echo(message, history):
-    answer = get_answer_from_chat_model(message)
+    answer = get_answer_from_chat_model(message, top_k=top_k)
     print(answer)
     for i in range(len(answer)):
         time.sleep(0.01)
@@ -14,8 +29,7 @@ def slow_echo(message, history):
 
 with gr.Blocks() as demo:
     gr.Markdown("# COM S 579 PDF ChatModel")
-    gr.Markdown("# Authors: Shuairan Chen, Fan Zhou, Jian Sun")
-    gr.Markdown("# Upload your PDF files and ask questions about the content.")
+    gr.Markdown(authors_text)
 
     with gr.Tab("Upload PDF"):
         gr.Markdown("# Upload and Indexing PDF file")
@@ -30,7 +44,16 @@ with gr.Blocks() as demo:
 
     with gr.Tab("Chat with Model"):
         gr.Markdown("# Chat with the PDF Chat Bot")
+        with gr.Column():
+            top_k_slider = gr.Slider(label="Top K", minimum=5, maximum=10, step=1)
+            update = gr.Button("Update Top K")
+            update.click(
+                fn=update_top_k,
+                inputs=top_k_slider
+            )
         chat_box = gr.ChatInterface(fn=slow_echo).queue()
+
+    top_k_slider.release(update_top_k, inputs=top_k_slider)
 
 
 if __name__ == "__main__":
--- user_query.py
+++ user_query.py
@@ -6,6 +6,7 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone
 from pinecone import ServerlessSpec
+import numpy as np
 
 tiktoken.encoding_for_model('gpt-3.5-turbo')
 tokenizer = tiktoken.get_encoding('cl100k_base')
@@ -32,29 +33,128 @@ def get_exists_index_from_pinecone(index_name):
     return None
 
 
-def create_chat_model(index):
+def get_document_by_id(doc_id):
+    index = get_exists_index_from_pinecone(index_name)
+    response = index.fetch(ids=[doc_id])
+    if 'vectors' in response and doc_id in response['vectors']:
+        document_data = response['vectors'][doc_id]
+        if 'metadata' in document_data and 'text' in document_data['metadata']:
+            document_path = document_data['metadata']['source']
+            document_name = os.path.basename(document_path)
+            doc_dict = {"document_name": document_name, "text": document_data['metadata']['text']}
+            return doc_dict
+    return None
+
+
+def retrieve_documents_from_pinecone(index_name, query_vector, top_k=5):
+    pinecone_index = get_exists_index_from_pinecone(index_name)
+    if pinecone_index is None:
+        raise ValueError(f"No index found with the name {index_name}")
+    # results = pinecone_index.query(queries=query_vector, top_k=top_k)
+    results = pinecone_index.query(vector=query_vector, top_k=top_k)
+
+    retrieved_doc_ids = [match["id"] for match in results.get('matches', [])]
+    retrieved_scores = {match["id"]: match["score"] for match in
+                        results.get('matches', [])}
+    retrieved_docs = [get_document_by_id(doc_id) for doc_id in retrieved_doc_ids]
+
+    return retrieved_docs, retrieved_scores
+
+
+def cosine_similarity(a, b):
+    dot_product = np.dot(a, b)
+    norm_a = np.linalg.norm(a)
+    norm_b = np.linalg.norm(b)
+    similarity = dot_product / (norm_a * norm_b)
+    return similarity
+
+
+def rerank_documents(query_vector, documents, top_p=5):
+    similarities = []
+    for doc in documents:
+        doc_vector = embedder.embed_documents(doc["text"])
+        if isinstance(doc_vector, list):
+            doc_vector = np.array(doc_vector)
+
+        if isinstance(doc_vector, np.ndarray) and doc_vector.ndim == 2:
+            doc_vector = np.mean(doc_vector, axis=0)  # Average the embeddings of the document
+
+        if isinstance(doc_vector, list):
+            doc_vector = np.array(doc_vector)
+
+        similarity = cosine_similarity(query_vector, doc_vector)
+        doc["similarity"] = similarity
+
+    sorted_data = sorted(documents, key=lambda x: x['similarity'], reverse=True)
+    top_p = min(top_p, len(documents))
+    top_p_documents = [sorted_data[i] for i in range(top_p)]
+
+    return top_p_documents
+
+
+def get_aggregated_docs(query_vector, top_k=5, top_p=5):
+    retrieved_docs, retrieved_scores = retrieve_documents_from_pinecone(index_name, query_vector, top_k=top_k)
+    ranked_docs = rerank_documents(query_vector, retrieved_docs, top_p=top_p)
+
+    referencing = ""
+    aggregated_docs = ""
+    for i in range(0, top_p):
+        doc = ranked_docs[i]
+        doc_name = doc["document_name"]
+        similarity = doc["similarity"]
+        titles = f"{i + 1}. From document {doc_name} (similarity={similarity}):\n"
+        text = doc["text"].replace("\n", " ")
+        referencing += f"{titles} {text}\n"
+        aggregated_docs += f"{text} "
+
+    return aggregated_docs, referencing
+
+
+def create_chat_model(index, top_k=5):
     text_field = "text"
     vectorstore = PineconeVectorStore(index, embedder, text_field)
-    retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 5})
+    retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": top_k})
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True, max_length=1000)
     model_kwargs = {
         "top_p": 0.9,
-        "frequency_penalty": 0.0,
-        "presence_penalty": 0.0
+        "frequency_penalty": 1.0,
+        "presence_penalty": 1.0
     }
-    llm = ChatOpenAI(api_key=openai_api_key, model_name="gpt-3.5-turbo", temperature=0.5, max_tokens=1000, model_kwargs=model_kwargs)
+    llm = ChatOpenAI(api_key=openai_api_key, model_name="gpt-3.5-turbo", temperature=0.5, max_tokens=1000,
+                     model_kwargs=model_kwargs)
     chat = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory)
     return chat
 
 
-def get_answer_from_chat_model(question):
+def normalized_query_vector(query_vector):
+    query_vector = np.array(query_vector, dtype=np.float32)
+
+    norm = np.linalg.norm(query_vector)
+    if norm == 0:
+        norm = 1
+
+    normalize_query_vector = query_vector / norm
+    return normalize_query_vector.tolist()
+
+
+def get_answer_from_chat_model(question, top_k=5):
     index = get_exists_index_from_pinecone(index_name)
-    chat = create_chat_model(index)
+    chat = create_chat_model(index, top_k)
+    query_vector = embedder.embed_query(question)
+    reference = ""
+
+    normalize_query_vector = normalized_query_vector(query_vector)
+    # print(normalize_query_vector)
+    aggregated_docs, refer = get_aggregated_docs(normalize_query_vector, top_k=top_k)
+    reference = "Reference:\n" + refer
+    combined_input = f"Question: {question}\n\n{reference}"
     dic = {
-        'question': question,
+        'question': combined_input
     }
+
     response = chat.invoke(dic)
     answer = response["answer"]
+    answer += "\n\n" + reference
     return answer
 
 
@@ -63,7 +163,10 @@ def command_running():
         print("Please input your question")
         print("User: ", end="")
         question = str(input())
-        answer = get_answer_from_chat_model(question)
+        print("Please input your question top_k value (5-10):", end="")
+        top_k = int(input())
+
+        answer = get_answer_from_chat_model(question, top_k)
         print("Chatbot:", answer)
 
         print("Do you want to ask another question? (y/n)")
